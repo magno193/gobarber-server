6a03b423c548e3dd6652b6ac59cc383b1bc10c58
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-22 19:59:25 -0300

    Config de Debug

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Server started! 😃' }));
+app.get('/', (request, response) =>
+  response.json({ message: 'Server started! 😃' }),
+);
 
 app.listen(3333, () => {
   console.log('🎖 Server started on port 3333');
