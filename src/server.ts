f8d82c446e242d04e8ccd586809f60458fd48c21
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-22 21:39:07 -0300

    Cadastro de agendamentos

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) =>
   response.json({ message: 'Server started! 😃' }),
 );
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🎖 Server started on port 3333');
 });
