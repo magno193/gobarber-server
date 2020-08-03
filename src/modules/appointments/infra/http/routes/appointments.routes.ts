7c7375499e389216a3acd57c06177147d82a1649
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-08-03 05:00:48 -0300

    Refatorando módulo de usuários

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -8,7 +8,6 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -19,6 +18,8 @@ appointmentsRouter.use(ensureAuthenticated);
 // });
 
 appointmentsRouter.post('/', async (request, response) => {
+  const appointmentsRepository = new AppointmentsRepository();
+
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
