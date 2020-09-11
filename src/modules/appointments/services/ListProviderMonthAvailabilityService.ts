27f98154d9f48302c983ddd699533fd702c07b95
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-09-11 12:44:31 -0300

    Fix: retorno de available em isAfter

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -50,7 +50,7 @@ class ListProviderMonthAvailabilityService {
       return {
         day,
         available:
-          isAfter(new Date(), compareDate) && appointmentsInDay.length < 10,
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
