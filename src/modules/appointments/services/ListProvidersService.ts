686c917b518a6fc6506c9a6dd0264617c3579c00
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-09-21 18:56:14 -0300

    Fix usando cache

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -19,11 +19,9 @@ class ListProviderService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    // let users = await this.cacheProvider.recover<User[]>(
-    //   `providers-list:${user_id}`,
-    // );
-
-    let users;
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
     if (!users) {
       users = await this.usersRepository.findAllProviders({
